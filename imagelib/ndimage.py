c4f8269e395076cff3277f80307d7f25026a09f3
Vincent van de Schaft <vincentvdschaft@gmail.com>
2025-10-09 08:45:27 +0200

    Update

--- imagelib/ndimage.py
+++ imagelib/ndimage.py
@@ -2,7 +2,10 @@ from pathlib import Path
 
 import matplotlib.image
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 
+from .dynamic_range import apply_dynamic_range_curve
+from .match_histograms import match_histograms
 from .ndextent import Extent
 from .saving import load_hdf5_image, save_hdf5_image
 
@@ -78,6 +81,10 @@ class NDImage:
         )
 
     @property
+    def pixel_sizes(self):
+        return [self.pixel_size(dim) for dim in range(self.ndim)]
+
+    @property
     def pixel_width(self):
         """Returns the pixel width."""
         return self.pixel_size(0)
@@ -86,6 +93,11 @@ class NDImage:
     def pixel_height(self):
         """Returns the pixel height."""
         return self.pixel_size(1)
+
+    @property
+    def extent_imshow(self):
+        """Returns the extent adjusted for imshow."""
+        return correct_extent_for_imshow(self.extent, self.shape)
 
     # ==========================================================================
     # Forward properties
@@ -105,6 +117,11 @@ class NDImage:
     @property
     def dtype(self):
         return self.array.dtype
+
+    @property
+    def T(self):
+        """Returns the transposed image."""
+        return NDImage(self.array.T, self.extent, metadata=self.metadata)
 
     # ==========================================================================
     # Metadata handling
@@ -222,6 +239,81 @@ class NDImage:
         assert path.suffix == ".hdf5", "File must be HDF5 format."
         return load_hdf5_image(path)
 
+    def with_array(self, array):
+        return NDImage(array, extent=self.extent, metadata=self.metadata)
+
+    def map_range(self, new_min, new_max, old_min=None, old_max=None):
+        """Map the image values to a new range [new_min, new_max]."""
+        if old_min is None:
+            old_min = np.min(self.array)
+        if old_max is None:
+            old_max = np.max(self.array)
+        scaled = (self.array - old_min) / (old_max - old_min)
+        mapped = scaled * (new_max - new_min) + new_min
+        return NDImage(mapped, self.extent, metadata=self.metadata)
+
+    def to_pixels(self):
+        """Convert the image to pixel values in the range [0, 1]."""
+        return self.map_range(0, 1)
+
+    def clip(self, min=None, max=None):
+        """Clip the image values to the given range."""
+        return NDImage(
+            np.clip(self.array, min, max), self.extent, metadata=self.metadata
+        )
+
+    def resample(self, shape, extent=None, method="linear", fill_value=0):
+        """Resample image to a new shape."""
+
+        if extent is None:
+            extent = self.extent
+        else:
+            extent = Extent(extent)
+
+        all_vals = [self.vals(dim) for dim in range(self.ndim)]
+
+        interpolator = RegularGridInterpolator(
+            all_vals,
+            self.array,
+            bounds_error=False,
+            fill_value=fill_value,
+            method=method,
+        )
+        new_all_vals = [
+            np.linspace(extent.start(dim), extent.end(dim), shape[dim])
+            for dim in range(len(shape))
+        ]
+
+        new_data = interpolator(np.meshgrid(*new_all_vals, indexing="ij")).reshape(
+            shape
+        )
+
+        return NDImage(
+            new_data,
+            extent=extent,
+            metadata=self.metadata,
+        )
+
+    def square_pixels(self):
+        nonzero_pixel_sizes = [
+            size for size in filter(lambda size: size > 0, self.pixel_sizes)
+        ]
+        new_pixel_size = min(nonzero_pixel_sizes)
+        print(f"new_pixel_size = {new_pixel_size}")
+
+        new_extent_initializer = []
+        new_shape = []
+        for dim in range(self.ndim):
+            new_n_pixels = int(self.extent.dim_size(dim) / new_pixel_size) + 1
+            new_shape.append(new_n_pixels)
+
+            new_extent_initializer.append(self.extent.start(dim))
+            new_extent_initializer.append(
+                self.extent.start(dim) + (new_n_pixels - 1) * new_pixel_size
+            )
+        new_extent = Extent(new_extent_initializer)
+        return self.resample(shape=new_shape, extent=new_extent, method="nearest")
+
     def get_window(self, extent: Extent):
         """Returns a new image that contains only the pixels in the window.
 
@@ -250,6 +342,111 @@ class NDImage:
 
         return self[tuple(slices)]
 
+    def match_histogram(self, other):
+        """Match the histogram of the image to another image."""
+
+        array = match_histograms(self.array, other.array)
+        return NDImage(array, extent=self.extent, metadata=self.metadata)
+
+    def apply_dynamic_range_curve(self, curve: np.ndarray):
+        """Apply a dynamic range curve to the image data."""
+        data = apply_dynamic_range_curve(curve, self.array)
+        return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+    def log_compress(self):
+        """Log-compress image data with 20*log10(image)."""
+
+        # Prevent taking the log of 0
+        data = np.where(self.array > 0, self.array, 1e-12)
+        data = 20 * np.log10(data)
+
+        return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+    def log_expand(self):
+        """Log-expand image data."""
+
+        array = np.power(10, self.array / 20)
+        array = np.where(self.array <= -240, 0, array)
+
+        return self.with_array(array)
+
+    def normalize(self, normval=None):
+        """Normalize image data by dividing by the max or normval."""
+
+        if normval is None:
+            normval = self.array.max()
+
+        if normval == 0.0:
+            print("Warning: normval is 0. Returning original image.")
+            return self
+
+        return self / normval
+
+    def normalize_db(self, normval=None):
+        """Normalize image data by adding the max or normval."""
+        if normval is None:
+            normval = self.array.max()
+
+        return self - np.array(normval)
+
+    def normalize_percentile(self, percentile=99):
+        """Normalize image data to the given percentile value."""
+        normval = np.percentile(self, percentile)
+        return self.normalize(normval)
+
+    # ==========================================================================
+    # Dunder methods
+    # ==========================================================================
+
+    def __add__(self, other):
+        """Add two images together."""
+        if isinstance(other, (int, float, np.number)):
+            data = self.array + other
+            return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+        if isinstance(other, NDImage):
+            assert all([e1 == e2 for e1, e2 in zip(self.extent, other.extent)])
+            data = self.array + other.array
+            return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+        other = np.array(other)
+        data = self.array + other
+        return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+    def __mul__(self, other):
+        """Multiply image."""
+        if isinstance(other, NDImage):
+            other = other.array
+
+        data = self.array * other
+        return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+    def __rmul__(self, other):
+        """Multiply image."""
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        """Divide image."""
+        if isinstance(other, NDImage):
+            other = other.array
+
+        data = self.array / other
+        return NDImage(data, extent=self.extent, metadata=self.metadata)
+
+    def __sub__(self, other):
+        """Subtract two images."""
+        return self + (other * -1)
+
+    def __eq__(self, other):
+        """Check if two images are equal."""
+        if not isinstance(other, NDImage):
+            return False
+
+        if self.extent != other.extent:
+            return False
+
+        return np.allclose(self.array, other.array)
+
 
 # ==============================================================================
 # Helper functions
@@ -264,3 +461,12 @@ def _check_ndimage_initializers(array, extent: Extent):
             assert extent.dim_size(dim) == 0.0, (
                 "Dimensions with one element must have zero size in the extent"
             )
+
+
+def correct_extent_for_imshow(extent: Extent, shape):
+    new_initializer = []
+    for dim in range(extent.ndim):
+        pixel_size = extent.dim_size(dim) / (shape[dim] - 1) if shape[dim] > 1 else 0.0
+        new_initializer.append(extent.start(dim) - pixel_size / 2)
+        new_initializer.append(extent.end(dim) + pixel_size / 2)
+    return Extent(new_initializer)
--- test_nd.py
+++ test_nd.py
@@ -1,13 +1,51 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from imagelib.ndextent import Extent
 from imagelib.ndimage import NDImage
 
-image = NDImage(np.ones((5, 5)), extent=(-1, 1, -1, 1), metadata={"author": "test"})
+image = NDImage(np.random.randn(4, 5, 5), extent=(0, 3, -1, 1, 0, 1))
+image + np.array(5)
+print(image.log_expand())
+image_normalized = image.normalize()
+
+# print(image.clip(0, 1).array)
+
+
+image_square = image.square_pixels()
+print(image_square)
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+axes[0].imshow(
+    image[0].log_compress().normalize_db().clip(-60, 0).T,
+    origin="lower",
+    extent=image.extent_imshow,
+    aspect="equal",
+)
+axes[1].imshow(
+    image_square.T, origin="lower", extent=image_square.extent_imshow, aspect="equal"
+)
+
+plt.show()
+print(image.square_pixels())
 print(image.get_window(extent=(-0.8, 0.8, -0.8, 0.8)))
 print(np.square(image).metadata)
 
+exit()
+image = NDImage(np.ones((25, 25)), extent=(-1, 1, -1, 1))
+grid = image.grid
+image = NDImage(
+    np.sin(np.pi * grid[..., 0]) * np.cos(np.pi * grid[..., 1]),
+    extent=(-1, 1, -1, 1),
+)
 
+resamples = image.resample(shape=(100, 100))
+print(resamples)
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+axes[0].imshow(image, origin="lower", extent=image.extent_imshow)
+axes[1].imshow(resamples, origin="lower", extent=resamples.extent_imshow)
+plt.show()
+
+print(image.resample(shape=(10, 10)))
 image[:3].save("test.hdf5")
 
 
