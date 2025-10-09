3d870f440f5ff3a2c6542ba1fb3343cec4f9bb4c
Vincent van de Schaft <vincentvdschaft@gmail.com>
2025-10-09 13:17:47 +0200

    Update

--- imagelib/__init__.py
+++ imagelib/__init__.py
@@ -1,8 +1,8 @@
-from .extent import Extent
-from .image import (
-    Image,
-    correct_imshow_extent,
+from .image_sequence import ImageSequence
+from .ndextent import Extent
+from .ndimage import NDImage as Image
+from .ndimage import (
+    correct_extent_for_imshow,
     load_hdf5_image,
     save_hdf5_image,
 )
-from .image_sequence import ImageSequence
